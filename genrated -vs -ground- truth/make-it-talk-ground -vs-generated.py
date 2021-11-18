119e4919aad981caec3e0e7a6ecd93e0c5234c1f
Raghav123-30 <59868685+Raghav123-30@users.noreply.github.com>
2021-11-18 10:25:13 +0530

    Update make-it-talk-ground -vs-generated.py

--- genrated -vs -ground- truth/make-it-talk-ground -vs-generated.py
+++ genrated -vs -ground- truth/make-it-talk-ground -vs-generated.py
@@ -6,7 +6,7 @@ while True:
     isTrue, frame = video.read()
     if isTrue == False:
         break
-    resized_frame = cv.resize(frame,(500,500))
+    resized_frame = cv.resize(frame,(256,256))
     cv.imwrite('Raghavendra/frames/'+str(i)+'.jpg', resized_frame)
 
     i += 1
